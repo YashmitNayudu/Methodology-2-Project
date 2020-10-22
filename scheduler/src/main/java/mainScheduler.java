e95d669bd99bbeade830389b70f6c4f3e14311e0
Purevsuren112 <54023895+Purevsuren112@users.noreply.github.com>
2020-10-22 10:40:12 +0200

    Add files via upload

---
+++ scheduler/src/main/java/mainScheduler.java
@@ -0,0 +1,14 @@
+
+public class mainScheduler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+       Employee obj1 = new Employee();
+       obj1.ifLate();
+       obj1.email();
+       }
+    
+}
