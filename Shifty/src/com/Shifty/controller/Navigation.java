d789f55c5928e4069cc4a982bbde5c98e2969e5c
BuildTools <yashmit.nayudu@gmail.com>
2020-11-06 11:14:35 +0100

    Made New Packages and added existing code

--- Shifty/src/com/Shifty/controller/Navigation.java
+++ Shifty/src/com/Shifty/controller/Navigation.java
@@ -5,10 +5,16 @@
  */
 
 package com.Shifty.controller;
+import com.Shifty.database.Database;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathExpressionException;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.xml.sax.SAXException;
 
 @Controller
 public class Navigation {
@@ -16,4 +22,5 @@ public class Navigation {
 	public ModelAndView LoginPage() {
 		return new ModelAndView("login");
 	}
+	
 }
